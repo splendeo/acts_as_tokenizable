742bbd6ee52d211afb567ca1a5e48e7010cae59f
Enrique García Cota <kikito@gmail.com>
2012-03-20 15:32:08 +0100

    the gem does not monkeypatch String any more

--- lib/acts_as_tokenizable/acts_as_tokenizable.rb
+++ lib/acts_as_tokenizable/acts_as_tokenizable.rb
@@ -1,37 +1,38 @@
+require 'acts_as_tokenizable/string_extensions'
+
 module ActsAsTokenizable
-  require 'acts_as_tokenizable/string_extensions'
-  
+
   # default to_token method. needs to have a "name" property on the object.
   # override for more complex token generation
   def to_token
     raise NoMethodError.new("You must redefine to_token in your model. Example: self.name.to_token()")
   end
-  
-  #makes self.token=self.to_token, in a convoluted way
+
+  #makes self.<token_field_name>=self.to_token
   def tokenize
     self.send("#{self.class.token_field_name}=", self.to_token)
   end
-  
+
   module ClassMethods
     attr_accessor :token_field_name
-    
+
     # search_token parameter is used by tokenized_by. This function allows for preparation
     # before tokenized_by function is invoked. Usually this means removing
     # stop words, replacing words.
     # By default it tokenizes each word and removes duplicates.
     def prepare_search_token(search_token)
-      search_token.words_to_token
+      StringExtensions::words_to_token(search_token)
     end
   end
-  
+
   def self.included(base)
     base.class_eval do
       extend ClassMethods
-      
+
       named_scope :tokenized_by, lambda {|search_token|
         search_strings = []
         search_values = []
-        prepare_search_token(search_token).words.each do |w|
+        StringExtensions::words(prepare_search_token(search_token)).each do |w|
           if w[0,1] == '-'
             search_strings.push("#{table_name}.#{token_field_name} NOT LIKE ?")
             search_values.push("%#{w[1,w.length]}%")
--- lib/acts_as_tokenizable/string_extensions.rb
+++ lib/acts_as_tokenizable/string_extensions.rb
@@ -1,66 +1,67 @@
-String.class_eval do
-  
-  #converts accented letters into ascii equivalents (i.e. ñ becomes n)
-  def normalize
-    #this version is in the forums but didnt work for me
-    #string = string.chars.normalize(:kd).gsub!(/[^\x00-\x7F]/n,'').to_s
-    mb_chars.normalize(:d).gsub(/[^\x00-\x7F]/n,'').to_s
-  end
-  
-  #returns true if numeric, false, otherwise
-  def numeric?
-    true if Float(self) rescue
-    return false
-  end
-  
-  #returns an array of strings containing the words on this string. removes spaces, strange chars, etc
-  def words
-    gsub(/[^\w|-]/, ' ').split
-  end
-  
-  #removes certain words from a string. 
-  # As a side-effect, all word-separators are converted to the separator char
-  def remove_words(words_array, separator = ' ')
-    (words - words_array).join separator
-  end
-  
-  # replaces certain words on a string. 
-  # As a side-effect, all word-separators are converted to the separator char
-  def replace_words(replacements, separator = ' ')
-    replaced_words = words
-    replacements.each do |candidates,replacement|
-      candidates.each do |candidate|
-        replaced_words=replaced_words.collect {|w| w==candidate ? replacement : w}
-      end
+module ActsAsTokenizable
+
+  module StringExtensions
+
+    #converts accented letters into ascii equivalents (i.e. ñ becomes n)
+    def self.normalize(str)
+      str.mb_chars.normalize(:d).gsub(/[^\x00-\x7F]/n,'').to_s
     end
-    replaced_words.join separator
-  end
-  
-  # returns an array that contains, in order:
-  #   * the numeric parts, converted to numbers
-  #   * the non-numeric parts, as text
-  # this is useful for sorting alphanumerically. For example:
-  # ["A1", "A12", "A2"].sort_by{|x| x.alphanumerics} => ["A1", "A2", "A12"]
-  #
-  # inspired by : http://blog.labnotes.org/2007/12/13/rounded-corners-173-beautiful-code/
-  def alphanumerics
-    split(/(\d+)/).map { |v| v =~ /\d/ ? v.to_i : v }
-  end
-  
-  #convert into something that can be used as an indexation key
-  def to_token(max_length=255)
-    string = self.normalize.strip.downcase.gsub(/[^\w|-]/, '') #remove all non-alphanumeric but hyphen (-)
-    string = string.squeeze unless string.numeric? #remove duplicates, except on pure numbers
-    return string[0..(max_length-1)]
-  end
-  
-  #convert into something that can be used on links
-  def to_slug(separator='-')
-    self.strip.downcase.normalize.words.join(separator)
-  end
-  
-  #tokenizes each word individually, and joins the word with the separator char.
-  def words_to_token(max_length=255, separator = ' ')
-    words.collect{|w| w.to_token}.uniq.join(separator)[0..(max_length-1)]
+
+    #returns true if numeric, false, otherwise
+    def self.numeric?(str)
+      true if Float(str) rescue
+      false
+    end
+
+    #returns an array of strings containing the words on this string. removes spaces, strange chars, etc
+    def self.words(str)
+      str.gsub(/[^\w|-]/, ' ').split
+    end
+
+    #removes certain words from a string.
+    # As a side-effect, all word-separators are converted to the separator char
+    def self.remove_words(str, words_array, separator = ' ')
+      (words(str) - words_array).join separator
+    end
+
+    # replaces certain words on a string. 
+    # As a side-effect, all word-separators are converted to the separator char
+    def self.replace_words(str, replacements, separator = ' ')
+      replaced_words = words(str)
+      replacements.each do |candidates,replacement|
+        candidates.each do |candidate|
+          replaced_words=replaced_words.collect {|w| w==candidate ? replacement : w}
+        end
+      end
+      replaced_words.join separator
+    end
+
+    # returns an array that contains, in order:
+    #   * the numeric parts, converted to numbers
+    #   * the non-numeric parts, as text
+    # this is useful for sorting alphanumerically. For example:
+    # ["A1", "A12", "A2"].sort_by{|x| x.alphanumerics} => ["A1", "A2", "A12"]
+    #
+    # inspired by : http://blog.labnotes.org/2007/12/13/rounded-corners-173-beautiful-code/
+    def self.alphanumerics(str)
+      str.split(/(\d+)/).map { |v| v =~ /\d/ ? v.to_i : v }
+    end
+
+    #convert into something that can be used as an indexation key
+    def self.to_token(str, max_length=255)
+      str = normalize(str).strip.downcase.gsub(/[^\w|-]/, '') #remove all non-alphanumeric but hyphen (-)
+      str = str.squeeze unless numeric?(str) #remove duplicates, except on pure numbers
+      return str[0..(max_length-1)]
+    end
+
+    #convert into something that can be used on links
+    def self.to_slug(str, separator='-')
+      words(normalize(str.strip.downcase)).join(separator)
+    end
+
+    #tokenizes each word individually, and joins the word with the separator char.
+    def self.words_to_token(str, max_length=255, separator = ' ')
+      words(str).collect{|w| to_token(w)}.uniq.join(separator)[0..(max_length-1)]
+    end
   end
 end
